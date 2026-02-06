01b0d8627907a67a5adc68238725a01cc936807f
Wasal Imtiaz <wasal.imtiaz@beetechy.com>
2026-02-06 16:21:09 +0500

    error added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,6 @@ from .views import health_check, get_data, home
 
 urlpatterns = [
     path('', home, name='home'),
-    path('health/', health_check, name='health_check'),
+    path('health/', health_chec, name='health_check'),
     path('data/', get_data, name='get_data'),
 ]
